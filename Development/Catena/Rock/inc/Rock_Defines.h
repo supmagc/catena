0daa854e4478d1cfc85f1d768bff9b5137444dfa
jelle <jelle@d67827bc-b4ba-4785-8dd8-7bf4e1788882>
2014-04-26 15:13:46 +0000

    Additional helper defines

--- Development/Catena/Rock/inc/Rock_Defines.h
+++ Development/Catena/Rock/inc/Rock_Defines.h
@@ -23,11 +23,19 @@
 
 #define _ERROR_WINDOWS_ONLY This code is only implemented to compile under windows.
 
+// Debug defines
 #define BREAK __asm { int 3 }
+
+// Helper defines
+#define ZERO(dest, length) memset((dest), 0, (length))
+
+// Delete/release defines
 #define SAFE_RELEASE(p) if(p != NULL) {p->Release(); p=NULL;}
 #define SAFE_rELEASE(p) if(p != NULL) {p->release(); p=NULL;}
 #define SAFE_DELETE(p) if(p != NULL) {delete p; p=NULL;}
 #define SAFE_DELETE_ARRAY(p) if(p != NULL) {delete[] p; p=NULL;}
+
+// Flag defines
 #define FLAG_HAS_ALL(n, f) (((n)&(f))==(f))
 #define FLAG_HAS_NOT(n, f) (((n)&(f))==0)
 #define FLAG_HAS_ONLY(n, f) ((n)==(f))
@@ -35,14 +43,19 @@
 #define FLAG_HAS(n, f) FLAG_HAS_ALL(n, f)
 #define FLAG_SET(n, f) (n|=(f))
 #define FLAG_CLEAR(n, f) (n&=~(f))
+
+// DLL defines
 #define DLL_EXPORT __declspec(dllexport)
 #define DLL_IMPORT __declspec(dllimport)
-#define ROCK_API DLL_EXPORT
 
+// Pimpl defines
 #define PIMPL_DECL(cls) struct cls##Impl; cls##Impl* m_pImpl
-#define PIMPL_INIT(cls) m_pImpl = new cls##Impl; ZeroMemory(m_pImpl, sizeof(cls##Impl))
+#define PIMPL_INIT(cls) m_pImpl = new cls##Impl; ZERO(m_pImpl, sizeof(cls##Impl))
 #define PIMPL_MAKE(nsp, cls) struct nsp##::##cls##::##cls##Impl
 #define PIMPL_DELETE() SAFE_DELETE(m_pImpl)
 #define PIMPL (*m_pImpl)
+
+// Rock specific defines
+#define ROCK_API DLL_EXPORT
 
 #endif // _H_ROCK_DEFINES
