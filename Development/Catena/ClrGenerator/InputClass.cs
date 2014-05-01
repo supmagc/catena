58a8a0b3db1c44bb3f2b9440f2b48d3a63172eaa
jelle <jelle@d67827bc-b4ba-4785-8dd8-7bf4e1788882>
2014-05-01 22:09:16 +0000

    ClrGenerator first pieces of code

---
+++ Development/Catena/ClrGenerator/InputClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClrGenerator {
+    
+    public class InputClass {
+
+        private List<InputMethod> m_lConstructors;
+        private List<InputMethod> m_lMethods;
+        private string[] m_aNamespaces;
+        private string m_sType;
+        private string m_sName;
+
+        public InputClass(string[] aNamespaces, string sType, string sName) {
+            m_aNamespaces = aNamespaces;
+            m_sType = sType;
+            m_sName = sName;
+        }
+
+        public void AddMethod(InputMethod oMethod) {
+            
+        }
+    }
+}
