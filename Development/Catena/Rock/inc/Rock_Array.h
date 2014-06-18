29aad8f3a3a87d1977bcfaffb80d3049d3dfeeeb
Jelle <supmagc@users.noreply.github.com>
2014-06-18 21:35:42 +0000

    small array stuff

--- Development/Catena/Rock/inc/Rock_Array.h
+++ Development/Catena/Rock/inc/Rock_Array.h
@@ -9,10 +9,32 @@ namespace Rock {
 
     template<typename T>
     class ROCK_API Array {
+    public:
+        Array(RUINT nCapacity, RUINT nBlockSize) : m_nLength(0), m_nCapacity(nCapacity), m_nBlockSize(nBlockSize) {
+            m_aData = new T[nCapacity];
+        }
+        Array(Array<T> const& oArray);
+        ~Array() {
+            SAFE_DELETE_ARRAY(m_aData);
+        }
 
+        void Push(Element mElement) {
+            m_aData[m_nLength++] = mElement;
+        }
+        Element Pop() {
+            return m_aData[--m_nLength];
+        }
+
+        Array<T>& operator=(Array<T> const& oArray);
+
+    private:
+        T* m_aData;
+        RUINT m_nLength;
+        RUINT m_nCapacity;
+        RUINT m_nBlockSize
     };
 
-    #include "../inl/Array.inl"
+    //#include "../inl/Array.inl"
 };
 
 #endif // _H_ROCK_ARRAY
