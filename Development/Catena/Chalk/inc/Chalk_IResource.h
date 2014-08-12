e5d0bf458da8f6db8d5b9323418b88911d0c5c13
jelle <jelle@d67827bc-b4ba-4785-8dd8-7bf4e1788882>
2014-08-12 10:44:25 +0000

    cmake changes for testing executables

--- Development/Catena/Chalk/inc/Chalk_IResource.h
+++ Development/Catena/Chalk/inc/Chalk_IResource.h
@@ -11,8 +11,8 @@ namespace Chalk {
     public:
         virtual void Release() =0;
         virtual IDevice* GetDevice() =0;
-        virtual IDevice const* GetDevice() const = 0;
-        virtual void OnDeviceLost() = 0;
+        virtual IDevice const* GetDevice() const =0;
+        virtual void OnDeviceLost() =0;
         virtual void OnDeviceReset() =0;
     };
 };
