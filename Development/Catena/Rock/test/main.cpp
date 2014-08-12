a5b034f816d134767f38a64647114cb1f7eaf535
Jelle <supmagc@users.noreply.github.com>
2014-08-12 10:44:25 +0000

    cmake changes for testing executables

--- Development/Catena/Rock/test/main.cpp
+++ Development/Catena/Rock/test/main.cpp
@@ -1,8 +1,40 @@
 
-#include <iostream>
+#include "Rock.h"
 using namespace std;
 
-void main()
-{
-    cout << "Hello World!" << endl;   cout << "Welcome to C++ Programming" << endl;
+class IParent {
+public:
+    virtual void Release() =0;
+};
+
+class IChild : public IParent {
+public:
+    virtual void Do() =0;
+};
+
+class Parent : public IParent {
+public:
+    virtual void Release() {
+        cout << "RELEASED !!" << endl;
+    }
+};
+
+class Child : public Parent, public IChild {
+public:
+    using Parent::Release;
+    virtual void Do() {
+        cout << "bla bla says the human" << endl;
+    }
+};
+
+int wmain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR pCmdLine, int nCmdShow) {
+    cout << "Hello World!" << endl << "Welcome to C++ Programming" << endl;
+
+    Child oChild = new Child();
+    oChild.Do();
+    oChild.Release();
+
+    char c;
+    cin >> c;
+    return 0;
 }
