9f14de2d4713cc8116f4ce4a8c78759317e563dd
jelle <jelle@d67827bc-b4ba-4785-8dd8-7bf4e1788882>
2014-08-24 17:39:12 +0000

    Console-stuff corrected

--- Development/Catena/Rock/test/main.cpp
+++ Development/Catena/Rock/test/main.cpp
@@ -30,10 +30,8 @@ public:
 */
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR pCmdLine, int nCmdShow) {
-	Console oConsole;
-	Console oConsoleTest;
-	oConsole.AttachToStdOut();
-	oConsole.SetColor(Console::Color::YELLOW, true, Console::Color::MAGENTA, false);
+    Console::Show();
+	Console::SetColor(Console::Color::YELLOW, true, Console::Color::MAGENTA, false);
 
 	cout << "Hello World!" << endl << "Welcome to C++ Programming" << endl;
 
